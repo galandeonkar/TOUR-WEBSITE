3a45e95eaf0c27b2bf1e800498ae575c03d0e90d
Onkar Galande <73571109+galandeonkar@users.noreply.github.com>
2023-04-17 14:05:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const helmet = require('helmet')
+const morgan = require('morgan');
+const AppError = require('./utils/appError')
+const errorHandler = require('./controllers/errorController');
+const cors = require('cors')
+
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const { default: rateLimit } = require('express-rate-limit');
+
+const app = express();
+
+app.use(helmet())
+app.use(cors())
+
+app.use(rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: true
+}))
+
+
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`))
+
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'not found',
+    //     message: `The route ${req.originalUrl} not found.`
+    // })
+
+    // const err = new Error(`The route ${req.originalUrl} was not found`)
+    // err.statusCode = 404
+    // err.status = 'not found'
+
+    next(new AppError(`The route ${req.originalUrl} was not found`, 404))
+})
+
+// Global Error Handler
+app.use(errorHandler)
+
+module.exports = app;
